6c8fa2587a72767fc3c71d0413ee1e22177d6053
jerkzilla <johnjamesmarshalljr@gmail.com>
2020-11-26 07:22:56 -0600

    added serializers

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,33 @@
 class PlayersController < ApplicationController
+
+    def index
+        @players = Player.all 
+        render json: @players
+    end
+
+    def create
+        @player = Player.new(player_params) 
+        if @player.save
+            render json: @player
+        else
+            render json: {error: 'Error creating player'}
+        end
+    end
+
+    def show
+        @player = Player.find(params[:id])
+        render json: @player
+    end
+
+    def destroy
+        @player = Player.find(params[:id]) 
+        @player.destroy
+    end
+
+    private
+
+    def player_params
+        params.require(:player).permit(:name, :school, :year, :scout)
+    end
+
 end
